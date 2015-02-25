351e1b5cd9f4c1d22b2899ffdfeef14f309e652a
Jeff Jenkinson <jeff.jenkinson@gmail.com>
2015-02-25 22:55:53 +0200

    Adding angular to src branch.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularSimpleValidationApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularSimpleValidationApp
+ */
+angular.module('angularSimpleValidationApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
